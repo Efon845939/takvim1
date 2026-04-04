9e8d46590ca35ab006776415df27a14136a65a57
Gamer Efe Türk Küçükvardar <proturkgamerefe@gmail.com>
2026-04-04 16:28:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5539002199-2f568",
+  "appId": "1:483558643376:web:aacf8ea2e0afd1b6a64d75",
+  "apiKey": "AIzaSyDJVLhfyhqgJdh8pI1rJLchf_N2JR-OuYQ",
+  "authDomain": "studio-5539002199-2f568.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "483558643376"
+};
